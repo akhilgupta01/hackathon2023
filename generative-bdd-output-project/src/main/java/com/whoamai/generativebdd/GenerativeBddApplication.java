bd9c4572e1a74a658f41e56956a287318650434f
Rajan Joshi <joshirajan999@gmail.com>
2023-07-14 14:00:48 +0530

    added new spring project

---
+++ generative-bdd-output-project/src/main/java/com/whoamai/generativebdd/GenerativeBddApplication.java
@@ -0,0 +1,13 @@
+package com.whoamai.generativebdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerativeBddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenerativeBddApplication.class, args);
+	}
+
+}
---
+++ generative-bdd-output-project/src/test/java/com/whoamai/generativebdd/GenerativeBddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whoamai.generativebdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenerativeBddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
